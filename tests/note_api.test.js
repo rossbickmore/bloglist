8129a02b50716d8c2a484934edf4e6900f0773c1
Ross Bickmore <rossbickmore@gmail.com>
2019-10-01 10:20:05 +0100

    completed step 2-3

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -6,12 +6,37 @@ const api = supertest(app)
 
 test('there are six blogs', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body.length).toBe(6)
+  expect(response.body.length).toBe(5)
 })
 
-test('the first note is about HTTP methods', async () => {
+test('the unique identifier property of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body[0].content).toBe("")
+  console.log(response.body[0].id)
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "What a silly billy",
+    author: "David Lithario",
+    url: "youtube.com",
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const title = response.body.map(r => r.title)
+
+  expect(response.body.length).toBe(7)
+  expect(title).toContain(
+    "What a silly billy"
+  )
 })
 
 
